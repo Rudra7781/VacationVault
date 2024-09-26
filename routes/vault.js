413a5cb0b6d88d6d67b781de815a766e3f42d983
Rudra <patelrudra2306@gmail.com>
2024-09-26 17:24:16 +1000

    adding vault routes

--- routes/vault.js
+++ routes/vault.js
@@ -1,12 +1,11 @@
-// routes for Vault
 const { Router } = require("express"); // Import the Router module from the Express framework to define route handlers.
 const multer = require("multer"); // Import Multer, a middleware for handling multipart/form-data, primarily for file uploads.
 const path = require("path"); // Import the path module to work with file and directory paths.
-
+ 
 const Vault = require("../models/vault"); // Import the Vault model to interact with the vault collection in MongoDB.
-
+ 
 const router = Router(); // Create a new Router instance to define the routes.
-
+ 
 const storage = multer.diskStorage({
   // Define storage options for Multer to specify where and how to save uploaded files.
   destination: function (req, file, cb) {
@@ -19,19 +18,37 @@ const storage = multer.diskStorage({
     cb(null, fileName); // Pass the file name to the callback function.
   },
 });
-
+ 
 const upload = multer({ storage: storage }); // Create a Multer instance with the defined storage options for handling file uploads.
-
+ 
+router.get("/add-new", (req, res) => {
+  // Define a route for the "Add New Vault" page.
+  return res.render("addVault", {
+    // Render the "addVault" view with the user data from the request.
+    user: req.user,
+  });
+});
+ 
+router.get("/:id", async (req, res) => {
+  // Define a route to fetch and display a specific vault by its ID.
+  const vault = await Vault.findById(req.params.id).populate("createdBy"); // Find the vault by ID and populate the "createdBy" field with user details.
+ 
+  return res.render("vault", {
+    // Render the "vault" view with the vault data and user data from the request.
+    user: req.user,
+    vault,
+  });
+});
 // Modify the route to handle multiple images upload
 router.post(
   "/",
   upload.fields([{ name: "coverImage" }, { name: "additionalImages" }]),
   async (req, res) => {
     const { title, body } = req.body;
-
+ 
     // Split the captions string into an array of captions
     let captions = req.body.captions ? req.body.captions.split(",") : [];
-
+ 
     const images = [];
     if (req.files.additionalImages) {
       req.files.additionalImages.forEach((file, index) => {
@@ -41,7 +58,7 @@ router.post(
         });
       });
     }
-
+ 
     const vault = await Vault.create({
       title,
       body,
@@ -49,9 +66,10 @@ router.post(
       coverImageURL: `/uploads/${req.files.coverImage[0].filename}`,
       images,
     });
-
+ 
     return res.redirect(`/vault/${vault._id}`);
   }
 );
-
+ 
 module.exports = router; // Export the router to be used in other parts of the application.
+ 
