49f3ac9b53f64b0f0c21faf99520bc175733fac1
AlanMaria <alanmaria2001@gmail.com>
2024-09-26 17:36:48 +1000

    Adding db model for vault

--- models/vault.js
+++ models/vault.js
@@ -1,1 +1,39 @@
-//vault db model will go here
+// Import the Schema and model functions from Mongoose to define and create a MongoDB model
+const { Schema, model } = require("mongoose");
+ 
+// Define the schema for the Vault model
+const vaultSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+    // Cover image for the vault
+    coverImageURL: {
+      type: String,
+      required: false,
+    },
+    // Array of additional images with captions
+    images: [
+      {
+        url: String,
+        caption: String,
+      },
+    ],
+    createdBy: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+ 
+const Vault = model("vault", vaultSchema);
+module.exports = Vault;
+ 
